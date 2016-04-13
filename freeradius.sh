b06ed7b312df89489584beea4abcdff55721ba79
galuhbeez <chris@galuhbeez.com>
2016-04-12 23:21:09 -0700

    added new commands to freeradius

--- freeradius.sh
+++ freeradius.sh
@@ -9,3 +9,13 @@
 #works good until this point
 
 sudo mysql -u root -ppassword -e "grant all on radius.* to radius@localhost identified by 'password'"
+
+
+#maunal commands work until this point
+
+#sudo mysql -u root -ppassword radius < /etc/freeradius/sql/mysql/schema.sql
+#sudo mysql -u root -ppassword radius < /etc/freeradius/sql/mysql/nas.sql
+
+
+mysql -u root -ppassword -e "use radius;INSERT INTO radcheck (UserName, Attribute, Value) VALUES ('sqltest', 'Password', 'testpwd');"
+
